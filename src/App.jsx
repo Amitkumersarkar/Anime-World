1e3482754637e26e7ace9b530552b40ca88a55ac
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-03-24 08:43:57 +0600

    added new file

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,61 @@ function App() {
           </div>
         </div>
         {/* </nav> */}
-        <div >
-          <img width="100%" src="Anime.PNG" alt="The Last Airbender" />
-        </div>
-        <div className='font-mono font-semibold pt-6 pb-10'>
-          <h2>Avatar : The Last Airbender</h2>
-          <h3>Rating : 9.3 </h3>
-          <h3>Creators : Michael Dante DiMartino and Brayn Konietzko </h3>
-          <p>
-            Storyline : The world is divided into four elemental nations: The Northern and Southern Water Tribes, the Earth Kingdom, the Fire Nation, and the Air Nomads. The Avatar upholds the balance between the nations, but everything changed when the Fire Nation invaded. Only the Avatar, master of all four elements, can stop them. But when the world needs him most, he vanishes. A hundred years later Katara and Sokka discover the new Avatar, an airbender named Aang. Together they must help Aang master the elements and save the world.
-          </p>
+        <div className='s2'>
+          {/* card-01 */}
+          <div className='font-mono font-semibold pt-6 pb-7 '>
+            <div className='pb-5'>
+              <img width="90%" src="Anime.PNG" alt="The Last Airbender" />
+            </div>
+            <h2>Avatar : The Last Airbender</h2>
+            <h3>Rating : 9.3 </h3>
+            <h3>Creators : Michael Dante DiMartino and Brayn Konietzko </h3>
+            <p>
+              Storyline : The world is divided into four elemental nations: The Northern and Southern Water Tribes, the Earth Kingdom, the Fire Nation, and the Air Nomads. The Avatar upholds the balance between the nations, but everything changed when the Fire Nation invaded. Only the Avatar, master of all four elements, can stop them. But when the world needs him most, he vanishes. A hundred years later Katara and Sokka discover the new Avatar, an airbender named Aang. Together they must help Aang master the elements and save the world.
+            </p>
+          </div>
+
+          {/* card-02 */}
+
+          <div className='font-mono font-semibold pt-6 pb-7'>
+            <div className='pb-5'>
+              <img width="90%" src="Anime.PNG" alt="The Last Airbender" />
+            </div>
+            <h2>Avatar : The Last Airbender</h2>
+            <h3>Rating : 9.3 </h3>
+            <h3>Creators : Michael Dante DiMartino and Brayn Konietzko </h3>
+            <p>
+              Storyline : The world is divided into four elemental nations: The Northern and Southern Water Tribes, the Earth Kingdom, the Fire Nation, and the Air Nomads. The Avatar upholds the balance between the nations, but everything changed when the Fire Nation invaded. Only the Avatar, master of all four elements, can stop them. But when the world needs him most, he vanishes. A hundred years later Katara and Sokka discover the new Avatar, an airbender named Aang. Together they must help Aang master the elements and save the world.
+            </p>
+          </div>
+
+          {/* card-03 */}
+
+          <div className='font-mono font-semibold pt-6 pb-7'>
+            <div className='pb-5'>
+              <img width="90%" src="Anime.PNG" alt="The Last Airbender" />
+            </div>
+            <h2>Avatar : The Last Airbender</h2>
+            <h3>Rating : 9.3 </h3>
+            <h3>Creators : Michael Dante DiMartino and Brayn Konietzko </h3>
+            <p>
+              Storyline : The world is divided into four elemental nations: The Northern and Southern Water Tribes, the Earth Kingdom, the Fire Nation, and the Air Nomads. The Avatar upholds the balance between the nations, but everything changed when the Fire Nation invaded. Only the Avatar, master of all four elements, can stop them. But when the world needs him most, he vanishes. A hundred years later Katara and Sokka discover the new Avatar, an airbender named Aang. Together they must help Aang master the elements and save the world.
+            </p>
+          </div>
+
+          {/* card-04 */}
+
+          <div className='font-mono font-semibold pt-6 pb-7'>
+            <div className='pb-5'>
+              <img width="90%" src="Anime.PNG" alt="The Last Airbender" />
+            </div>
+            <h2>Avatar : The Last Airbender</h2>
+            <h3>Rating : 9.3 </h3>
+            <h3>Creators : Michael Dante DiMartino and Brayn Konietzko </h3>
+            <p>
+              Storyline : The world is divided into four elemental nations: The Northern and Southern Water Tribes, the Earth Kingdom, the Fire Nation, and the Air Nomads. The Avatar upholds the balance between the nations, but everything changed when the Fire Nation invaded. Only the Avatar, master of all four elements, can stop them. But when the world needs him most, he vanishes. A hundred years later Katara and Sokka discover the new Avatar, an airbender named Aang. Together they must help Aang master the elements and save the world.
+            </p>
+          </div>
         </div>
       </div>
     </>
